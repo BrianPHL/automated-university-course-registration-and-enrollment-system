42dd5b3556a1c9c04d5d4562afaea651a07bcb06
Brian <brianpasco1206@gmail.com>
2024-11-21 23:22:30 +0800

    feat(src/js): add inputsHandler method

--- src/main.js
+++ src/main.js
@@ -23,6 +23,7 @@ $(() => {
 
         console.log("[DEBUG]: Starting initialization of modules in main app.js!");
         themeHandler();
+        inputsHandler();
 
     }
 
@@ -80,6 +81,41 @@ $(() => {
 
         });
 
+        debug("done", functionName)
+
+    }
+
+    const inputsHandler = () => {
+
+        const functionName = "inputsHandler";
+
+        debug("init", functionName)
+
+        $('.form-control').on('focus', function() {
+            $(this).closest('.input-group').addClass('focused');
+        }).on('blur', function() {
+            $(this).closest('.input-group').removeClass('focused');
+        });
+
+        $('.togglePassword').on('click', function() {
+
+            const passwordField = $(this).closest('.input-group').find('.form-control');
+            const passwordFieldType = passwordField.attr('type');
+            
+            if (passwordFieldType === 'password') {
+
+                passwordField.attr('type', 'text');
+                $(this).removeClass('fa-eye').addClass('fa-eye-slash');
+
+            } else {
+
+                passwordField.attr('type', 'password');
+                $(this).removeClass('fa-eye-slash').addClass('fa-eye');
+
+            }
+
+        })
+        
         debug("done", functionName)
 
     }
