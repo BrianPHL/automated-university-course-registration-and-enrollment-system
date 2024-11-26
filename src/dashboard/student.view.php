aeb98c5ecb0508ce52d930ca2f9bf5b8cc86cbb7
Brian <brianpasco1206@gmail.com>
2024-11-26 21:08:29 +0800

    feat(public): add dashboard for each role

--- src/dashboard/student.view.php
+++ src/dashboard/student.view.php
@@ -1,7 +1,43 @@
 <?php
 
-    if (!isset($_SESSION)) {
-        session_start();
-    }
+    if (!isset($_SESSION)) { session_start(); }
 
 ?>
+
+<!DOCTYPE html>
+
+<html lang="en" data-theme="dark">
+
+    <head>
+
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/bootstrap/4.6.2/css/bootstrap.min.css" integrity="sha512-rt/SrQ4UNIaGfDyEXZtNcyWvQeOq0QLygHluFQcSjaGB04IxWhal71tKuzP6K8eYXYB6vJV4pHkXcmFGGQ1/0w==" crossorigin="anonymous" referrerpolicy="no-referrer" />
+        <link id="favicon" rel="shortcut icon" href="../../../public/favicon-light.ico" type="image/x-icon">
+        <link rel="stylesheet" href="./css/dashboard.css">
+        <title>student dashboard | aucres</title>
+
+    </head>
+
+    <body>
+        
+        <div class="container">
+
+            <h1>Hello, world!</h1>
+
+        </div>
+
+        <section id="footer">
+
+            <div class="container">
+
+                <h6>&copy; 2024 Automated University Course Registration and Enrollment System. All rights reserved.</h6>
+                <i class="theme-toggle fa-solid fa-moon"></i>
+
+            </div>
+
+        </section>
+
+    </body>
+
+</html>
