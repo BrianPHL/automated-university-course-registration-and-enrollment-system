2cc326ccae452883b3e4aaa0f2acef1479a83566
Brian <brianpasco1206@gmail.com>
2024-11-26 11:38:37 +0800

    chore(public/js): cleanup auth.js

--- public/js/auth.js
+++ public/js/auth.js
@@ -13,13 +13,11 @@ $(() => {
         
         if (passwordFieldType === 'password') {
     
-            console.log("toggled");
             passwordField.attr('type', 'text');
             $(this).removeClass('fa-eye-slash').addClass('fa-eye');
     
         } else {
     
-            console.log("untoggled");
             passwordField.attr('type', 'password');
             $(this).removeClass('fa-eye').addClass('fa-eye-slash');
     
