02e0ab73b1ee78263baf3579080b1a5e8d07a06e
Brian <brianpasco1206@gmail.com>
2024-12-02 02:40:18 +0800

    feat(public): add function guard clause

--- public/dashboard.php
+++ public/dashboard.php
@@ -8,6 +8,12 @@
 
     function getEntriesCount($pConn, $pTable, $pRole) {
 
+        $conn = (isset($pConn)) ? $pConn : null;
+        $table = (isset($pTable)) ? $pTable : null;
+        $role = (isset($pRole)) ? $pRole : null;
+
+        if (empty($conn) || empty($table) || empty($role)) return;
+
         $stmt = $pConn->prepare("SELECT COUNT(*) AS total_entries FROM $pTable WHERE role = :role");
         $stmt->bindParam(":role", $pRole);
         $stmt->execute();
@@ -20,9 +26,17 @@
 
     function getTableData($pConn, $pTable, $pRole = null) {
 
-        $sql = (empty($pRole)) ? "SELECT * FROM $pTable" : "SELECT * FROM $pTable WHERE role = $pRole";
+        $conn = (isset($pConn)) ? $pConn : null;
+        $table = (isset($pTable)) ? $pTable : null;
+        $role = (isset($pRole)) ? $pRole : null;
+        $sql;
 
+        if (empty($conn) || empty($table)) return;
+        
+        $sql = (empty($role)) ? "SELECT * FROM $pTable" : "SELECT * FROM $pTable WHERE role = :role";
+        
         $stmt = $pConn->prepare($sql);
+        if (isset($role)) { $stmt->bindParam(":role", $role); }
         $stmt->execute();
         $results = $stmt->fetchAll(PDO::FETCH_ASSOC);
 
