1964ad3854da62c1fda798ec9fc8b03f0f411f6c
Brian <brianpasco1206@gmail.com>
2024-12-01 15:45:52 +0800

    chore(src): code cleanup

--- src/dashboard/admin.view.php
+++ src/dashboard/admin.view.php
@@ -247,21 +247,10 @@
 
         </section>
 
-        <!-- <section id="footer">
-
-            <div class="container">
-
-                <h6>&copy; 2024 Automated University Course Registration and Enrollment System. All rights reserved.</h6>
-                <i class="theme-toggle fa-solid fa-moon"></i>
-
-            </div>
-
-        </section> -->
-
         <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.7.1/jquery.min.js" integrity="sha512-v2CJ7UaYy4JwqLDIrZUI/4hqeoQieOmAZNXBeQyjo21dadnwR+8ZaIJVT8EE2iyI61OV8e6M8PP2/4hpQINQ/g==" crossorigin="anonymous" referrerpolicy="no-referrer"></script>
         <script src="https://cdnjs.cloudflare.com/ajax/libs/bootstrap/4.6.2/js/bootstrap.bundle.min.js" integrity="sha512-igl8WEUuas9k5dtnhKqyyld6TzzRjvMqLC79jkgT3z02FvJyHAuUtyemm/P/jYSne1xwFI06ezQxEwweaiV7VA==" crossorigin="anonymous" referrerpolicy="no-referrer"></script>
 
-        <!-- <script type="module" src="./js/globals.js"></script> -->
+        <script type="module" src="./js/globals.js"></script>
         <script type="module" src="./js/dashboard.js"></script>
         <script type="module" src="//cdn.datatables.net/2.1.8/js/dataTables.min.js"></script>
 
